ad57a62b8e6555766e2295f39bcc6588de0703b2
s26049 <s26049@gsm.hs.kr>
2026-08-10 04:01:42 +0900

    연사 등록에 JsonFormat 추가

--- src/main/java/com/teamnative/relayplus/domain/lecture/dto/LectureCreateRequest.java
+++ src/main/java/com/teamnative/relayplus/domain/lecture/dto/LectureCreateRequest.java
@@ -1,5 +1,6 @@
 package com.teamnative.relayplus.domain.lecture.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -24,6 +25,7 @@ public record LectureCreateRequest(
 
         @NotNull(message = "연사 날짜와 시간을 입력해주세요.")
         @Future(message = "연사 날짜와 시간은 현재보다 이후여야 합니다.")
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime scheduledAt,
 
         @NotNull(message = "모집 인원을 입력해주세요.")
